4002ca539f7312d9438a46740b01310eee1d1127
shangshenzhen <18839133063@163.com>
2023-08-17 14:27:10 +0800

    注释

--- service-user-app/src/main/java/com/ssz/user/binlog/handler/UserEventHandler.java
+++ service-user-app/src/main/java/com/ssz/user/binlog/handler/UserEventHandler.java
@@ -1,9 +1,12 @@
 package com.ssz.user.binlog.handler;
 
+import com.alibaba.fastjson.JSON;
 import com.ssz.user.binlog.module.BinLogItem;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserEventHandler implements CommonEventHandler {
 
     @Override
@@ -13,16 +16,16 @@ public class UserEventHandler implements CommonEventHandler {
 
     @Override
     public void insertHandle(BinLogItem binLogItem) {
-        System.out.println("user add。。。。。。。。");
+        log.info("user表 添加数据:{}", JSON.toJSONString(binLogItem));
     }
 
     @Override
     public void updateHandle(BinLogItem binLogItem) {
-
+        log.info("user表 编辑数据:{}", JSON.toJSONString(binLogItem));
     }
 
     @Override
     public void deleteHandle(BinLogItem binLogItem) {
-
+        log.info("user表 删除数据:{}", JSON.toJSONString(binLogItem));
     }
 }
